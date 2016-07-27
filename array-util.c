9025a0af30020a944f36cf66cda677f756556e9b
Martin Wind <mawi1988@gmail.com>
2016-07-27 18:42:22 +0200

    add bit size filter

--- array-util.c
+++ array-util.c
@@ -17,20 +17,22 @@
 // Define all variables at the beginning to make the C99 compiler
 // happy.
 int main(int argc, char **argv) {
-	mpz_array s, m, b, uniques;
+	mpz_array s, m, b, uniques, filtered;
 	mpz_t sum_bits, avg;
 	size_t count, i, j, x, size, size_min = 0, size_max = 0;
-	int c, vflg = 0, iflg = 0, sflg = 0, lflg = 0, bflg = 0, cflg = 0, uflg = 0, errflg = 0, r = 0;
+	int c, vflg = 0, iflg = 0, sflg = 0, lflg = 0, bflg = 0, cflg = 0, uflg = 0, tflg = 0, xflg = 0, errflg = 0, r = 0;
 	char *filename = "primes.lst";
 	char *out_filename = NULL;
 	char chunk_name[MAX_CHUNK_NAME_LENGTH];
 	long int length = 0;
 	long int seek = 0;
+	long int tolerance = 0;
+	long int bitsize = 0;
 	unsigned int padding = 9;
 
 	// #### argument parsing
 	// Boring `getopt` argument parsing.
-	while ((c = getopt(argc, argv, ":vsicub:l:o:")) != -1) {
+	while ((c = getopt(argc, argv, ":vsicux:t:b:l:o:")) != -1) {
 		switch(c) {
 		case 'o':
 			out_filename = optarg;
@@ -49,6 +51,14 @@ int main(int argc, char **argv) {
 			break;
 		case 'c':
 			cflg++;
+			break;
+		case 'x':
+			xflg++;
+			bitsize = strtol(optarg, NULL, 0);
+			break;
+		case 't':
+			tflg++;
+			tolerance = strtol(optarg, NULL, 0);
 			break;
 		case 'l':
 			lflg++;
@@ -82,15 +92,17 @@ int main(int argc, char **argv) {
 	// Print the usage and exit if an error occurred during argument parsing.
 	if (errflg) {
 		fprintf(stderr, "usage: [-vs] [-o FILE] [file]\n"\
-                        "\n\t-i        inspect the array"\
-                        "\n\t-o FILE   the output file"\
+						"\n\t-i        inspect the array"\
+						"\n\t-o FILE   the output file"\
 						"\n\t-l length max values to output or chunk size"\
 						"\n\t-b count  skip first count (seek)"\
 						"\n\t-c        create chunks"\
-                        "\n\t-v        be more verbose"\
-                        "\n\t-s        sort the input"\
+						"\n\t-v        be more verbose"\
+						"\n\t-s        sort the input"\
 						"\n\t-u        count uniques"\
-                        "\n\n");
+						"\n\t-x bits   only output integers with size bits"\
+						"\n\t-t bits   tolerance in bits for -x"\
+						"\n\n");
 		exit(2);
 	}
 
@@ -130,6 +142,21 @@ int main(int argc, char **argv) {
 		array_unique(&uniques, &s);
 		printf("unique: %zu / %zu\n", uniques.used, s.used);
 		array_clear(&uniques);
+	}
+
+	if (xflg > 0) {
+		if (vflg > 0)
+			printf("filter %zu integers by size %zu bits +/- %zu bits\n", s.used, bitsize, tolerance);
+
+		array_init(&filtered, 10);
+		for(i=0; i<s.used; i++) {
+			size = mpz_sizeinbase(s.array[i], 2);
+			if (size <= (bitsize + tolerance) && size >= (bitsize - tolerance)) {
+				array_add(&filtered, s.array[i]);
+			}
+		}
+		s = filtered;
+
 	}
 
 	if (iflg > 0) {
@@ -197,7 +224,6 @@ int main(int argc, char **argv) {
 				}
 			}
 			array_clear(&b);
-
 		} else {
 			// just process seek and length
 			if (vflg > 0)
